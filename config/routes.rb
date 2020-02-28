d554d04e29f106433666bf2627c4004f869fdffe
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-02-28 14:48:08 -0300

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api, path: '/' do
+  namespace :api, path: '/api' do
     resources :users
   end
+
+  resources :home, only: %i[index]
+  root to: 'home#index'
+
 end
